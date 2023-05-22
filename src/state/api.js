bb48e12e4f950be8c72041e563ef1fd7a673198c
isaac-svg <isaacsakyi009@gmail.com>
2023-05-22 19:52:18 +0000

    commit

--- src/state/api.js
+++ src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4040" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.BASE_URL }),
   reducerPath: "adminApi",
   tagTypes: [
     "User",
